33e4621ae43b01ac2d4f362af9c435a84d8d39ea
Darryl-Shi <221474B@student.hci.edu.sg>
2024-06-20 01:16:50 +0800

    Update run-all-scripts.py

--- run-all-scripts.py
+++ run-all-scripts.py
@@ -1,16 +1,11 @@
 import os
-import subprocess
 
 def execute_bash_scripts(directory_path):
+    for root, dirs, files in os.walk(directory_path):
+        for filename in files:
+            if filename.endswith(".sh"):
+                script_path = os.path.join(root, filename)
+                os.system(f"bash {script_path}")
 
-    for filename in os.listdir(directory_path):
-        file_path = os.path.join(directory_path, filename)
-        if os.path.isfile(file_path) and filename.endswith(".sh"):
-            try:
-                subprocess.run(["bash", file_path], check=True)
-                print(f"Executed {filename}")
-            except subprocess.CalledProcessError as e:
-                print(f"Error executing {filename}: {e}")
-
-subdirectory_path = "scripts"
+subdirectory_path = "./scripts"
 execute_bash_scripts(subdirectory_path)
